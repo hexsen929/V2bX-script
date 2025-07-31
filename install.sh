9aa4ebf578e9f82a0e80de40066b7b9799cbe787
hexsen <759071793@qq.com>
2025-08-01 00:11:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -103,18 +103,18 @@ install_base() {
 
 # 0: running, 1: not running, 2: not installed
 check_status() {
-    if [[ ! -f /usr/local/wordpress_proxy/wordpress ]]; then
+    if [[ ! -f /usr/local/myapp/myapp ]]; then
         return 2
     fi
     if [[ x"${release}" == x"alpine" ]]; then
-        temp=$(service wordpress_proxy status | awk '{print $3}')
+        temp=$(service myapp status | awk '{print $3}')
         if [[ x"${temp}" == x"started" ]]; then
             return 0
         else
             return 1
         fi
     else
-        temp=$(systemctl status wordpress_proxy | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+        temp=$(systemctl status myapp | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
         if [[ x"${temp}" == x"running" ]]; then
             return 0
         else
@@ -123,55 +123,95 @@ check_status() {
     fi
 }
 
-install_wordpress_proxy() {
-    if [[ -e /usr/local/wordpress_proxy/ ]]; then
-        rm -rf /usr/local/wordpress_proxy/
+install_upx() {
+    # 检查 UPX 是否已经安装
+    if ! command -v upx &> /dev/null; then
+        echo -e "${yellow}未检测到 UPX，正在安装 UPX...${plain}"
+        # 判断发行版并安装 UPX
+        if [[ x"${release}" == x"centos" || x"${release}" == x"redhat" ]]; then
+            yum install -y upx
+        elif [[ x"${release}" == x"ubuntu" || x"${release}" == x"debian" ]]; then
+            apt-get update && apt-get install -y upx-ucl
+        elif [[ x"${release}" == x"alpine" ]]; then
+            apk add upx
+        else
+            echo -e "${red}不支持的系统，无法安装 UPX！${plain}"
+            exit 1
+        fi
+    else
+        echo -e "${green}UPX 已经安装，跳过安装步骤。${plain}"
+    fi
+}
+
+install_myapp() {
+    if [[ -e /usr/local/myapp/ ]]; then
+        rm -rf /usr/local/myapp/
     fi
 
-    mkdir /usr/local/wordpress_proxy/ -p
-    cd /usr/local/wordpress_proxy/
+    mkdir /usr/local/myapp/ -p
+    cd /usr/local/myapp/
 
     if  [ $# == 0 ] ;then
         last_version=$(curl -Ls "https://api.github.com/repos/wyx2685/V2bX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}检测 wordpress_proxy 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 wordpress_proxy 版本安装${plain}"
+            echo -e "${red}检测 myapp 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 myapp 版本安装${plain}"
             exit 1
         fi
-        echo -e "检测到 wordpress_proxy 最新版本：${last_version}，开始安装"
-        wget --no-check-certificate -N --progress=bar -O /usr/local/wordpress_proxy/wordpress-linux.zip https://github.com/wyx2685/V2bX/releases/download/${last_version}/V2bX-linux-${arch}.zip
+        echo -e "检测到 myapp 最新版本：${last_version}，开始安装"
+        wget --no-check-certificate -N --progress=bar -O /usr/local/myapp/myapp-linux.zip https://github.com/wyx2685/V2bX/releases/download/${last_version}/V2bX-linux-${arch}.zip
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 wordpress_proxy 失败，请确保你的服务器能够下载 Github 的文件${plain}"
+            echo -e "${red}下载 myapp 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
         fi
     else
         last_version=$1
         url="https://github.com/wyx2685/V2bX/releases/download/${last_version}/V2bX-linux-${arch}.zip"
-        echo -e "开始安装 wordpress_proxy $1"
-        wget --no-check-certificate -N --progress=bar -O /usr/local/wordpress_proxy/wordpress-linux.zip ${url}
+        echo -e "开始安装 myapp $1"
+        wget --no-check-certificate -N --progress=bar -O /usr/local/myapp/myapp-linux.zip ${url}
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 wordpress_proxy $1 失败，请确保此版本存在${plain}"
+            echo -e "${red}下载 myapp $1 失败，请确保此版本存在${plain}"
             exit 1
         fi
     fi
 
-    unzip wordpress-linux.zip
-    rm wordpress-linux.zip -f
-    chmod +x V2bX
-    mv V2bX wordpress  # Rename the file to 'wordpress'
+    unzip myapp-linux.zip
+    rm myapp-linux.zip -f
+    chmod +x myapp
+    mkdir /etc/myapp/ -p
+    cp geoip.dat /etc/myapp/
+    cp geosite.dat /etc/myapp/
 
-    # Compress with UPX
-    upx /usr/local/wordpress_proxy/wordpress
-    chmod +x /usr/local/wordpress_proxy/wordpress
+    # 压缩 myapp 文件
+    install_upx
+    upx /usr/local/myapp/myapp
 
-    mkdir /etc/wordpress_proxy/ -p
-    cp geoip.dat /etc/wordpress_proxy/
-    cp geosite.dat /etc/wordpress_proxy/
+    if [[ x"${release}" == x"alpine" ]]; then
+        rm /etc/init.d/myapp -f
+        cat <<EOF > /etc/init.d/myapp
+#!/sbin/openrc-run
 
-    # Systemd service for wordpress_proxy
-    rm /etc/systemd/system/wordpress_proxy.service -f
-    cat <<EOF > /etc/systemd/system/wordpress_proxy.service
+name="myapp"
+description="myapp"
+
+command="/usr/local/myapp/myapp"
+command_args="server"
+command_user="root"
+
+pidfile="/run/myapp.pid"
+command_background="yes"
+
+depend() {
+        need net
+}
+EOF
+        chmod +x /etc/init.d/myapp
+        rc-update add myapp default
+        echo -e "${green}myapp ${last_version}${plain} 安装完成，已设置开机自启"
+    else
+        rm /etc/systemd/system/myapp.service -f
+        cat <<EOF > /etc/systemd/system/myapp.service
 [Unit]
-Description=wordpress_proxy Service
+Description=myapp Service
 After=network.target nss-lookup.target
 Wants=network.target
 
@@ -183,35 +223,94 @@ LimitAS=infinity
 LimitRSS=infinity
 LimitCORE=infinity
 LimitNOFILE=999999
-WorkingDirectory=/usr/local/wordpress_proxy/
-ExecStart=/usr/local/wordpress_proxy/wordpress server
+WorkingDirectory=/usr/local/myapp/
+ExecStart=/usr/local/myapp/myapp server
 Restart=always
 RestartSec=10
 
 [Install]
 WantedBy=multi-user.target
 EOF
+        systemctl daemon-reload
+        systemctl stop myapp
+        systemctl enable myapp
+        echo -e "${green}myapp ${last_version}${plain} 安装完成，已设置开机自启"
+    fi
 
-    systemctl daemon-reload
-    systemctl stop wordpress_proxy
-    systemctl enable wordpress_proxy
-    echo -e "${green}wordpress_proxy ${last_version}${plain} 安装完成，已设置开机自启"
+    # 后续配置复制
+    if [[ ! -f /etc/myapp/config.json ]]; then
+        cp config.json /etc/myapp/
+        echo -e ""
+        echo -e "全新安装，请先参看教程：https://myapp.example.com/，配置必要的内容"
+        first_install=true
+    else
+        if [[ x"${release}" == x"alpine" ]]; then
+            service myapp start
+        else
+            systemctl start myapp
+        fi
+        sleep 2
+        check_status
+        echo -e ""
+        if [[ $? == 0 ]]; then
+            echo -e "${green}myapp 重启成功${plain}"
+        else
+            echo -e "${red}myapp 可能启动失败，请稍后使用 myapp log 查看日志信息，若无法启动，则可能更改了配置格式，请前往 wiki 查看：https://github.com/myapp-project/myapp/wiki${plain}"
+        fi
+        first_install=false
+    fi
 
+    if [[ ! -f /etc/myapp/dns.json ]]; then
+        cp dns.json /etc/myapp/
+    fi
+    if [[ ! -f /etc/myapp/route.json ]]; then
+        cp route.json /etc/myapp/
+    fi
+    if [[ ! -f /etc/myapp/custom_outbound.json ]]; then
+        cp custom_outbound.json /etc/myapp/
+    fi
+    if [[ ! -f /etc/myapp/custom_inbound.json ]]; then
+        cp custom_inbound.json /etc/myapp/
+    fi
+    curl -o /usr/bin/myapp -Ls https://raw.githubusercontent.com/wyx2685/myapp-script/master/myapp.sh
+    chmod +x /usr/bin/myapp
+    if [ ! -L /usr/bin/myapp ]; then
+        ln -s /usr/bin/myapp /usr/bin/myapp
+        chmod +x /usr/bin/myapp
+    fi
     cd $cur_dir
     rm -f install.sh
     echo -e ""
-    echo "wordpress_proxy 管理脚本使用方法:"
+    echo "myapp 管理脚本使用方法 (兼容使用myapp执行，大小写不敏感): "
     echo "------------------------------------------"
-    echo "wordpress_proxy start - 启动 wordpress_proxy"
-    echo "wordpress_proxy stop - 停止 wordpress_proxy"
-    echo "wordpress_proxy restart - 重启 wordpress_proxy"
-    echo "wordpress_proxy status - 查看 wordpress_proxy 状态"
-    echo "wordpress_proxy enable - 设置 wordpress_proxy 开机自启"
-    echo "wordpress_proxy disable - 取消 wordpress_proxy 开机自启"
-    echo "wordpress_proxy log - 查看 wordpress_proxy 日志"
+    echo "myapp              - 显示管理菜单 (功能更多)"
+    echo "myapp start        - 启动 myapp"
+    echo "myapp stop         - 停止 myapp"
+    echo "myapp restart      - 重启 myapp"
+    echo "myapp status       - 查看 myapp 状态"
+    echo "myapp enable       - 设置 myapp 开机自启"
+    echo "myapp disable      - 取消 myapp 开机自启"
+    echo "myapp log          - 查看 myapp 日志"
+    echo "myapp x25519       - 生成 x25519 密钥"
+    echo "myapp generate     - 生成 myapp 配置文件"
+    echo "myapp update       - 更新 myapp"
+    echo "myapp update x.x.x - 更新 myapp 指定版本"
+    echo "myapp install      - 安装 myapp"
+    echo "myapp uninstall    - 卸载 myapp"
+    echo "myapp version      - 查看 myapp 版本"
     echo "------------------------------------------"
+    # 首次安装询问是否生成配置文件
+    if [[ $first_install == true ]]; then
+        read -rp "检测到你为第一次安装myapp,是否自动直接生成配置文件？(y/n): " if_generate
+        if [[ $if_generate == [Yy] ]]; then
+            curl -o ./initconfig.sh -Ls https://raw.githubusercontent.com/wyx2685/myapp-script/master/initconfig.sh
+            source initconfig.sh
+            rm initconfig.sh -f
+            generate_config_file
+        fi
+    fi
 }
 
 echo -e "${green}开始安装${plain}"
 install_base
-install_wordpress_proxy $1
+install_myapp $1
